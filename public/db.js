abfd1fe7374120aef54c1e144762137c73ed4f2a
Jeffrey Harding <jeffreyh202@gmail.com>
2020-10-12 11:28:52 -0600

    service worker updates

--- public/db.js
+++ public/db.js
@@ -49,6 +49,5 @@ function checkDatabase() {
           }
         };
       }
-    
-
+window.addEventListener("online", checkDatabase);
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -7,22 +7,21 @@ const FILES_TO_CACHE = [
     '/db.js'
   ];
   
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
+  const CACHE1 = 'cache-v1';
+  const RUNTIME = 'runtime_cache';
   
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
+  self.addEventListener('install', (e) => {
+    e.waitUntil(
       caches
-        .open(PRECACHE)
+        .open(CACHE1)
         .then((cache) => cache.addAll(FILES_TO_CACHE))
         .then(self.skipWaiting())
     );
   });
   
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
+  self.addEventListener('activate', (e) => {
+    const currentCaches = [CACHE1, RUNTIME];
+    e.waitUntil(
       caches
         .keys()
         .then((cacheNames) => {
@@ -39,17 +38,17 @@ const FILES_TO_CACHE = [
     );
   });
   
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
+  self.addEventListener('fetch', (e) => {
+    if (e.request.url.startsWith(self.location.origin)) {
+      e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
           if (cachedResponse) {
             return cachedResponse;
           }
   
           return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
+            return fetch(e.request).then((response) => {
+              return cache.put(e.request, response.clone()).then(() => {
                 return response;
               });
             });
